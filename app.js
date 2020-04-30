190a878fb18212763301aa2e12bdc83f86ee8a20
TehDrink <kesterteh@gmail.com>
2020-04-30 22:13:13 +0800

    Commit: Fixed sql statements & pagination errors. Added comment labels for better understanding on what does what. Proceed with frontend integration.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+//Start server via "npm start"
+//This database uses ElephantSQL that uses PostgreSQL as a service.
+
+//Require statements
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//app.use statements
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,11 +17,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const database = require('./database')
-
-app.post('/insertAdvertisement/', function (req,res, next){
+//POST 1. Insert Advertisement details
+app.post('/insertAdvertisement/', function (req, res, next) {
   const { data } = req.body;
-  database.insertAdvertisment(data, (error, result) =>{
-    if(error){
+  database.insertAdvertisement(data, (error, result) => {
+    if (error) { //Return error if error.
       console.log("data: ", data)
       return next(error);
     }
@@ -26,10 +30,11 @@ app.post('/insertAdvertisement/', function (req,res, next){
   });
 });
 
-app.get('/basic/Alldata', function(req, res, next){
+//GET 2. Retrieve all data
+app.get('/basic/Alldata', function (req, res, next) {
   const { companyId, audienceReach, page, pageSize } = req.query;
   database.getData(companyId, audienceReach, page, pageSize, (error, result) => {
-    if(error){
+    if (error) {
       return next(error);
     }
     res.json(result);
@@ -37,20 +42,20 @@ app.get('/basic/Alldata', function(req, res, next){
 });
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch error 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({
+  res.json({ //Not sending a html file, thus we send a json.
     error: err.message,
     code: err.status || 500,
   });
