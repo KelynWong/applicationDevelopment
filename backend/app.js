ea7d7c920e4c77cfbbf0069f9e2b4f8c17dc9edc
KelynWong <kelynwonget@gmail.com>
2020-07-07 18:44:15 +0800

    modified table, manually putting in adType for api, additional advance api

--- backend/app.js
+++ backend/app.js
@@ -27,10 +27,10 @@ app.options('*', cors());
 
 const database = require('./database')
 const algo = require('./algo')
-// POST 1. Insert Advertisement details ---
+// POST 1. BASIC Insert Advertisement details ---
 app.post('/basic/insert/', function (req, res, next) {
   const { data } = req.body;
-  database.insertAdvertisement(data, (error, result) => {
+  database.basicInsertAdvertisement(data, (error, result) => {
     if (error) { //Return error if error.
       console.log("data: ", data)
       res.json({"error": error.detail, "code": error.code});
@@ -41,7 +41,21 @@ app.post('/basic/insert/', function (req, res, next) {
   });
 });
 
-// POST 2. BASIC Retrieve all data ---
+// POST 2. ADVANCE Insert Advertisement details ---
+app.post('/advance/insert/', function (req, res, next) {
+  const { data } = req.body;
+  database.advInsertAdvertisement(data, (error, result) => {
+    if (error) { //Return error if error.
+      console.log("data: ", data)
+      res.json({"error": error.detail, "code": error.code});
+      // return next(error);
+    }
+    console.log(result)
+    res.json({"result": "success"});
+  });
+});
+
+// POST 3. BASIC Retrieve all data ---
 app.post('/basic/allData', function (req, res) {
   console.log(req.body);
   const { companyId, audienceReach, pageNo, pageSize } = req.body;
@@ -58,7 +72,7 @@ app.post('/basic/allData', function (req, res) {
   })
 });
 
-// POST 2. ADVANCED Retrieve all data ---
+// POST 4. ADVANCED Retrieve all data ---
 app.post('/advanced/allData', function (req, res) {
   console.log(req.body);
   const { companyId, audienceReach, pageNo, pageSize } = req.body;
@@ -75,7 +89,7 @@ app.post('/advanced/allData', function (req, res) {
   })
 });
 
-// GET 3. BASIC: Retrieve all number of row data in table: advertisement ---
+// GET 5. BASIC: Retrieve all number of row data in table: advertisement ---
 app.post('/extra/basicGetRowCount', function (req, res, next) {
   const { companyId, audienceReach } = req.body;
   database.basicGetRowCount(companyId, audienceReach, (error, result) => {
@@ -89,7 +103,7 @@ app.post('/extra/basicGetRowCount', function (req, res, next) {
   })
 });
 
-// GET 3. ADVANCED: Retrieve all number of row data in table: advertisement ---
+// GET 6. ADVANCED: Retrieve all number of row data in table: advertisement ---
 app.post('/extra/advGetRowCount', function (req, res, next) {
   const { companyId, audienceReach } = req.body;
   database.advGetRowCount(companyId, audienceReach, (error, result) => {
@@ -105,7 +119,7 @@ app.post('/extra/advGetRowCount', function (req, res, next) {
 
 
 // Result Viewer API
-// POST 4. Retrieve Basic data for chart ---
+// POST 7. Retrieve BASIC data for chart ---
 app.post('/basic/allChartData', function (req, res) { 
   console.log(req.body);
   const { optionIds } = req.body; 
@@ -147,8 +161,111 @@ app.post('/basic/allChartData', function (req, res) {
   })
 });
 
-// POST 5. Get BASIC tabulation results ---
+// POST 8. Retrieve ADVANCE data for chart ---
+app.post('/advance/allChartData', function (req, res) { 
+  console.log(req.body);
+  const { optionIds } = req.body; 
+
+  database.basicGetDataForChart(optionIds, (error, result) => {  // links to database.js
+    if(result.length == 0){
+      return res.json({"error": "Not found", "code": "404"}); //No optionId
+    }else if (error) {
+      return res.json({"error": error.detail, "code": error.code}); //Other errors
+      // return next(error);
+    }
+      // Adding boolean
+      var optionType = [];
+      for (let i = 0; i < result.length; i++) {
+        optionType.push(result[i].adtype)
+      }
+      //The check
+      var same = true;
+      // for (var j = 1; j < optionType.length; j++) {
+      //   if(optionType[0] == optionType[j]){
+      //     same = true;
+      //   }else{
+      //     same = false;
+      //   }
+      // }
+
+      //The for loop eventually returns true or false but theres 2 issues:
+      // 1. Lets say theres 5 ids inputted, and no. 4 is the different adType.
+      // The var same will still = true at the end :/
+      // 2. Lets say we fix no.1, the final result is false yes, but it does not specify 
+      // which option was the outlier.
+      // So we gotta fix no. 1 and also make the error able to identify what id is causing it.
+      if(same == false){
+        return res.json({"error": "Different ad type", "code": "404"});
+      }else if(same == true){
+        console.log(result);
+        res.json(result);
+      }
+  })
+});
+
+// POST 9. Get BASIC tabulation results ---
 app.post('/basic/result', function (req, res, next) {
+  const { optionIds, budget } = req.body;
+  console.log("optionids: " + optionIds)
+  console.log("budget: " + budget)
+  database.basicGetOptionsForComputation(optionIds, (error, result) => { // links to database.js
+    if(result.length == 0){
+      return res.json({"error": "Not found", "code": "404"});
+    }else if (error) {
+      return res.json({"error": error.detail, "code": error.code});
+      // return next(error);
+    }
+      var audience = [];
+      var cost = [];
+      var optionType = []
+      for (let i = 0; i < result.length; i++) {
+        audience.push(result[i].audiencereach)
+        cost.push(Number(result[i].cost))
+        optionType.push(result[i].adtype)
+      }
+      var same = true;
+      // for (var j = 1; j < optionType.length; j++) {
+      //   if(optionType[0] == optionType[j]){
+      //     same = true
+      //   }else{
+      //     same = false
+      //   }
+      // }
+      // if(same == false){
+      //   return res.json({"error": "Different ad type", "code": "404"});
+      // }else{
+        var updatedCost= []
+        var payment = []
+        // if(optionType[0]=="Not Fixed" && same== true){
+          updatedCost = algo.fractionalKnapsack(audience, cost, budget);
+          for (let k = 0; k < result.length; k++) {
+            console.log("Number(result[k].cost): " +Number(result[k].cost))
+            if(parseInt(result[k].cost) == parseInt(updatedCost[k])){
+              payment.push('Full')
+            }else if(parseInt(updatedCost[k]) == 0){
+              payment.push('No')
+              result[k].audiencereach = 0
+            }else{
+              payment.push('Partial')
+              var factor = result[k].cost/updatedCost[k]
+              var updatedAudience = result[k].audiencereach/factor
+              result[k].audiencereach = updatedAudience
+            }
+            result[k].cost = updatedCost[k]
+            result[k]['payment']=payment[k]
+          }
+        // }
+        // else if(optionType[0]=="Fixed" && same== true){
+        //   var n = result.length
+        //   // console.log("RESULTS OF ALGO: "+ algo.fullKnapsack(audience, cost, budget, n))
+        // }
+        res.json(result)
+        //}
+  })
+});
+
+// POST 10. Get ADVANCE tabulation results ---
+app.post('/advance/result', function (req, res, next) {
   const { optionIds, budget } = req.body;
   console.log("optionids: " + optionIds)
   console.log("budget: " + budget)
